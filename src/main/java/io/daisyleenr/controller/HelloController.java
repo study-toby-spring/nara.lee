e818d0cf7f0388c9070da3d9228dd6795ffb1012
Nara Lee <daisyleenr@gmail.com>
2017-08-16 17:54:28 +0900

    @RequestParam 설정 추가

--- src/main/java/io/daisyleenr/controller/HelloController.java
+++ src/main/java/io/daisyleenr/controller/HelloController.java
@@ -3,6 +3,7 @@ package io.daisyleenr.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -10,7 +11,7 @@ public class HelloController {
 
     @RequestMapping(path="/hello", method={RequestMethod.GET, RequestMethod.POST})
     @ResponseBody
-    public String hello(){
-        return "hello, world";
+    public String hello(@RequestParam String name){
+        return String.format("hello, %s", name);
     }
 }
