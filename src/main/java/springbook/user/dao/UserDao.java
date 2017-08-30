b7901d11873cf3d0b1cd6481c5f97f7b49e25c86
Nara Lee <daisyleenr@gmail.com>
2017-08-31 00:13:56 +0900

    [1.1] 초난감 DAO - JDBC를 통해 DB에 User를 저장하고 조회하는 간단한 DAO 작성 - 테스트 코드 작성

---
+++ src/main/java/springbook/user/dao/UserDao.java
@@ -0,0 +1,75 @@
+package springbook.user.dao;
+
+import springbook.user.domain.User;
+
+import java.sql.*;
+
+/**
+ * 1. DB 연결을 위한 Connection을 가져온다.
+ * 2. SQL을 담은 Statement(PreparedStatement)를 만든다.
+ * 3. 만들어진 Statement를 실행한다.
+ * 4. 조회의 경우 SQL 쿼리의 실행 결과를 ResultSet으로 받아서 정보를 저장한다.
+ * 5. 작업 중에 생성된 리소스는 작업을 마친 후 반드시 닫아준다.
+ */
+public class UserDao {
+    public void add(User user) throws ClassNotFoundException, SQLException{
+        Class.forName("com.mysql.jdbc.Driver");
+        Connection c = DriverManager.getConnection("jdbc:mysql://localhost/toby_spring", "root", "");
+
+        PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values(?,?,?)");
+        ps.setString(1, user.getId());
+        ps.setString(2, user.getName());
+        ps.setString(3, user.getPassword());
+
+        ps.executeUpdate();
+
+        ps.close();
+        c.close();
+    }
+
+    public User get(String id) throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.jdbc.Driver");
+        Connection c = DriverManager.getConnection("jdbc:mysql://localhost/toby_spring", "root", "");
+
+        PreparedStatement ps = c.prepareStatement("select * from users where id = ?");
+        ps.setString(1, id);
+
+        ResultSet rs = ps.executeQuery();
+        rs.next();
+
+        User user = new User();
+        user.setId(rs.getString("id"));
+        user.setName(rs.getString("name"));
+        user.setPassword(rs.getString("password"));
+
+        rs.close();
+        ps.close();
+        c.close();
+
+        return user;
+    }
+
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        UserDao userDao = new UserDao();
+
+        User user = new User();
+        user.setId("100");
+        user.setName("이나라");
+        user.setPassword("비밀번호");
+
+        userDao.add(user);
+        System.out.printf(user.getId() + "등록 성공");
+
+        User user2 = userDao.get(user.getId());
+        System.out.println(user2.getName() + ", " + user2.getPassword() + " 조회 성공");
+    }
+
+    /**
+     * 왜 이 코드에 문제가 많다고 하는 것일까?
+     * 잘 동작하는 코드를 굳이 수정하고 개선해야 하는 이유는 뭘까?
+     * 그렇게 DAO 코드를 개선했을 때의 장점은 무엇인가?
+     * 그런 장점들이 당장에, 또는 미래에 주는 유익은 무엇인가?
+     * 또, 객체지향 설계의 원칙과는 무슨 상관이 있을까?
+     * 이 DAO를 개선하는 경우와 그대로 사용하는 경우, 스프링을 사용하는 개발에서 무슨 차이가 있을까?
+     */
+}
