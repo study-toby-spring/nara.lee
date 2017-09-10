4fb19d78d76c3f6070deafdeec4d5e4414373deb
Nara Lee <daisyleenr@gmail.com>
2017-09-11 07:15:15 +0900

    [1.8] XML을 이용한 설정 - XML을 이용하는 애플리케이션 컨텍스트 추가 - 애플리케이션 컨텍스트가 DaoFactory 대신 XML 설정정보를 활용하도록 수정

--- src/main/java/springbook/user/dao/UserDaoTest.java
+++ src/main/java/springbook/user/dao/UserDaoTest.java
@@ -1,18 +1,18 @@
 package springbook.user.dao;
 
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
 import springbook.user.domain.User;
 
 import java.sql.SQLException;
 
 public class UserDaoTest {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class); // @Configuration
+        ApplicationContext context = new GenericXmlApplicationContext("context/application-context.xml");
         UserDao dao  = context.getBean("userDao", UserDao.class); //@Bean
 
         User user = new User();
-        user.setId("104");
+        user.setId("111");
         user.setName("이나라");
         user.setPassword("비밀번호");
 
@@ -21,22 +21,5 @@ public class UserDaoTest {
 
         User user2 = dao.get(user.getId());
         System.out.println(user2.getName() + ", " + user2.getPassword() + " 조회 성공");
-
-        //////////////////////////
-        // identity vs equality
-        //////////////////////////
-
-        DaoFactory factory = new DaoFactory();
-        UserDao dao1 = factory.userDao();
-        UserDao dao2 = factory.userDao();
-
-        System.out.println(dao1);
-        System.out.println(dao2);
-
-        UserDao dao3  = context.getBean("userDao", UserDao.class); //@Bean
-        UserDao dao4  = context.getBean("userDao", UserDao.class); //@Bean
-
-        System.out.println(dao3);
-        System.out.println(dao4);
     }
 }
