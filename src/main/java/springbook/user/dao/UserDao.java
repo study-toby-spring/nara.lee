44109bf82f35a2f403afbeb6c75bb256dfba8623
Nara Lee <daisyleenr@gmail.com>
2017-09-16 11:59:14 +0900

    [3.2] DI 적용을 위한 클라이언트/컨텍스트 분리

--- src/main/java/springbook/user/dao/UserDao.java
+++ src/main/java/springbook/user/dao/UserDao.java
@@ -60,34 +60,8 @@ public class UserDao {
     }
 
     public void deleteAll() throws SQLException {
-        Connection c = null;
-        PreparedStatement ps = null;
-
-        try {
-            c = dataSource.getConnection();
-
-            StatementStrategy strategy = new DeleteAllStatement();
-            ps = strategy.makePreparedStatement(c);
-
-            ps.executeUpdate();
-        }catch (SQLException e){
-            throw e;
-        }finally {
-            if(ps != null){
-                try{
-                    ps.close();
-                }catch (SQLException e){ }
-            }
-
-            if(c != null){
-                try{
-                    c.close();
-                }catch (SQLException e){ }
-            }
-        }
-
-        ps.close();
-        c.close();
+        StatementStrategy st = new DeleteAllStatement();
+        jdbcContextWithStatementStrategy(st);
     }
 
     public int getCount() throws SQLException {
@@ -116,6 +90,31 @@ public class UserDao {
                 try{
                     c.close();
                 }catch (SQLException e){ }
+            }
+        }
+    }
+
+    public void jdbcContextWithStatementStrategy(StatementStrategy stmt) throws SQLException{
+        Connection c = null;
+        PreparedStatement ps = null;
+
+        try{
+            c = dataSource.getConnection();
+            ps = stmt.makePreparedStatement(c);
+            ps.executeUpdate();
+        }catch (SQLException e){
+            throw e;
+        }finally {
+            if(ps != null){
+                try{
+                    ps.close();
+                }catch (SQLException e){}
+            }
+
+            if(c != null){
+                try{
+                    c.close();
+                }catch (SQLException e){}
             }
         }
     }
