b5ebedc91a1120dceff58551f61af29653af74c7
Nara Lee <nrlee@whatap.io>
2017-11-07 21:25:17 +0900

    [5] 서비스 추상화 - 이넘 필드 추가 및 DB 스키마 변경 - 사용자 수정 기능 추가 (update)

--- src/main/java/springbook/user/dao/UserDaoJdbc.java
+++ src/main/java/springbook/user/dao/UserDaoJdbc.java
@@ -3,6 +3,7 @@ package springbook.user.dao;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+import springbook.user.domain.Level;
 import springbook.user.domain.User;
 
 import javax.sql.DataSource;
@@ -24,13 +25,16 @@ public class UserDaoJdbc implements UserDao {
                     user.setId(resultSet.getString("id"));
                     user.setName(resultSet.getString("name"));
                     user.setPassword(resultSet.getString("password"));
+                    user.setLevel(Level.valueOf(resultSet.getInt("level")));
+                    user.setLogin(resultSet.getInt("login"));
+                    user.setRecommend(resultSet.getInt("recommend"));
                     return user;
                 }
             };
 
     public void add(final User user) throws DuplicateKeyException {
-        this.jdbcTemplate.update("insert into users(id, name, password) values(?,?,?)",
-                user.getId(), user.getName(), user.getPassword());
+        this.jdbcTemplate.update("insert into users(id, name, password, level, login, recommend) values(?,?,?,?,?,?)",
+                user.getId(), user.getName(), user.getPassword(), user.getLevel().intValue(), user.getLogin(), user.getRecommend());
     }
 
     public User get(String id){
@@ -57,10 +61,19 @@ public class UserDaoJdbc implements UserDao {
         return this.jdbcTemplate.queryForObject("select count(*) from users", Integer.class);
     }
 
+    public void update(User user) {
+//        this.jdbcTemplate.update(
+//                "update users set name = ?, password = ?, level = ?, login = ?, recommend = ? where id = ? ",
+//                user.getName(), user.getPassword(), user.getLevel().intValue(), user.getLogin(), user.getRecommend(), user.getId());
+
+        this.jdbcTemplate.update(
+                "update users set name = ?, password = ?, level = ?, login = ?, recommend = ? ",
+                user.getName(), user.getPassword(), user.getLevel().intValue(), user.getLogin(), user.getRecommend());
+    }
+
     public List<User> getAll() {
         return this.jdbcTemplate.query("select * from users order by id", this.userMapper);
     }
-
 
     /**
      * 왜 이 코드에 문제가 많다고 하는 것일까?
--- src/test/java/UserDaoTest.java
+++ src/test/java/UserDaoTest.java
@@ -7,6 +7,7 @@ import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import springbook.user.dao.UserDao;
+import springbook.user.domain.Level;
 import springbook.user.domain.User;
 
 import java.sql.SQLException;
@@ -29,9 +30,9 @@ public class UserDaoTest {
 
     @Before
     public void setUp(){
-        user1 = new User("gyumee", "박성철", "springno1");
-        user2 = new User("leegw700", "이길원", "springno2");
-        user3 = new User("bumjin", "박범진", "springno3");
+        user1 = new User("gyumee", "박성철", "springno1", Level.BASIC, 1, 0);
+        user2 = new User("leegw700", "이길원", "springno2", Level.SILVER, 55, 10);
+        user3 = new User("bumjin", "박범진", "springno3", Level.GOLD, 100, 40);
     }
 
     @Test
@@ -43,9 +44,11 @@ public class UserDaoTest {
         dao.add(user2);
         assertThat(dao.getCount(), is(2));
 
+        User userget1 = dao.get(user1.getId());
+        checkSameUser(userget1, user1);
+
         User userget2 = dao.get(user2.getId());
-        assertThat(userget2.getName(), is(user2.getName()));
-        assertThat(userget2.getPassword(), is(user2.getPassword()));
+        checkSameUser(userget2, user2);
     }
 
     @Test
@@ -101,6 +104,9 @@ public class UserDaoTest {
         assertThat(user1.getId(), is(user2.getId()));
         assertThat(user1.getName(), is(user2.getName()));
         assertThat(user1.getPassword(), is(user2.getPassword()));
+        assertThat(user1.getLevel(), is(user2.getLevel()));
+        assertThat(user1.getLogin(), is(user2.getLogin()));
+        assertThat(user1.getRecommend(), is(user2.getRecommend()));
     }
 
     @Test(expected = DataAccessException.class)
@@ -108,5 +114,25 @@ public class UserDaoTest {
         dao.deleteAll();
         dao.add(user1);
         dao.add(user1);
+    }
+
+    @Test
+    public void update(){
+        dao.deleteAll();
+
+        dao.add(user1);
+        dao.add(user2);
+
+        user1.setName("오민규");
+        user1.setPassword("springno6");
+        user1.setLevel(Level.GOLD);
+        user1.setLogin(10000);
+        user1.setRecommend(999);
+        dao.update(user1);
+
+        User user1update = dao.get(user1.getId());
+        checkSameUser(user1, user1update);
+        User user2update = dao.get(user2.getId());
+        checkSameUser(user2, user2update);
     }
 }
